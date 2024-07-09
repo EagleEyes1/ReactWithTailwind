567ca34c404b18a6495f580d05c330725b698fec
unknown <fahder7@gmail.com>
2024-07-09 18:48:53 +0700

    implement tailwind UI to React Project

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import ProductList from "../components/ProductList";
+import Footer from "../components/Footer";
+
+const LandingPage = () => {
+  return (
+    <>
+      <HeroSection />
+      <ProductList />
+      <Footer />
+    </>
+  );
+};
+
+export default LandingPage;
